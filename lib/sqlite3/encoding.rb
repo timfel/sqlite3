d0ff9fb2fd991c5574e69a9d5c747b478893a002
Tim Felgentreff <timfelgentreff@gmail.com>
2011-01-24 11:46:20 +0100

    add ruby 1.8 fallback support

--- lib/sqlite3/encoding.rb
+++ lib/sqlite3/encoding.rb
@@ -1,16 +1,42 @@
 module SQLite3
   class Encoding
+
+    if RUBY_VERSION =~ /1.8/
+      # Add missing methods and constants for 1.8
+      class ::String
+        def force_encoding(enc); self; end
+        def encode(enc); self; end
+        def encoding; $KCODE; end
+      end
+
+      ASCII_8BIT = "N"
+      US_ASCII = "N"
+      UTF_8 = "U"
+      UTF_16BE = "N"
+      UTF_16LE = "N"
+      ::Encoding = Encoding
+    end
+
     class << self
       def find(encoding)
         enc = encoding.to_s
         if enc.downcase == "utf-16"
           utf_16native
         else
+          return find18(enc) if Encoding == ::Encoding
           ::Encoding.find(enc).tap do |e|
             if utf_16?(e) && e != utf_16native
               raise ArgumentError, "requested to use byte order different than native"
             end
           end
+        end
+      end
+
+      def find18(enc)
+        if ["us-ascii", "utf-8"].include? enc.downcase
+          send(enc.downcase.sub("-", "_").to_sym)
+        else
+          raise ArgumentError, "requested encoding not supported on #{RUBY_VERSION}"
         end
       end
 
